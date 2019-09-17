d8b248c6e79cc40b9ba5d2392ac95da586e53b1e
Stephanie Davidson <rpookie98@gmail.com>
2019-09-17 01:08:13 -0400

    Got the game working to a win condition;

--- gameEngine.py
+++ gameEngine.py
@@ -8,22 +8,20 @@ assert sys.version_info >= version, "This script requires at least Python {0}.{1
 logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
+#Players need to Shower, Brush Teeth, Take Medicine, Wash Face, Do Makeup, Style Hair, and Get Dressed, in that order
 
 
 # Game loop functions
-def render(game,current,moves,points):
-    ''' Displays the current room, moves, and points '''
+def render(game,current,moves):
+    ''' Displays the current room, moves '''
     r = game['rooms']
     c = r[current]
 
-    print('\n\nMoves: {moves}, Points: {points}'.format(moves=moves, points=points))
-    print('\n\nYou are in {name}'.format(name=c['name']))
+    print('\n\nMoves: {moves}'.format(moves=moves))
+    print('\n\n{name}'.format(name=c['name']))
     print(c['desc'])
-    if len(c['inventory']):
-        print('You see the following items:')
-        for i in c['inventory']:
-            print('\t{i}'.format(i=i))
+    if len(c['inventory'])>0:
+        print('You have done the following things:')
 
 def getInput(game,current,verbs):
     ''' Asks the user for input and normalizes the inputted value. Returns a list of commands '''
@@ -37,18 +35,114 @@ def getInput(game,current,verbs):
 
 def update(selection,game,current,inventory):
     ''' Process the input and update the state of the world '''
-    s = list(selection)[0]  #We assume the verb is the first thing typed
+    s = list(selection)[0]  #We assume the verb is the first thing typed\
+    
     if s == "":
         print("\nSorry, I don't understand.")
         return current
+
     elif s == 'EXITS':
         printExits(game,current)
         return current
+
+#Actions needed to complete game; genuinely couldn't get them to work in the json file, so they're here ¯\_(ツ)_/¯
+    elif current == "SHOWER" and s == "BATHE":
+        if len(inventory) == 0:
+            inventory.append("SHOWERED")
+            print("\nYou successfully TAKE a SHOWER\nNothing beats a shower after a night of too much drinking in a self-destructive streak!")
+        else:
+            print("\nYou already showered! No need to raise your water bill any higher than it is.")
+
+    elif current == "SINK" and s == "TEETH":
+        if len(inventory) == 1:
+            inventory.append("BRUSHED TEETH")
+            print("\nYou successfully BRUSH you TEETH\nYou already start to feel better.")
+        elif len(inventory) == 0:
+            print("\nYou know you can technically brush your teeth before showering, but it just feels weird.")
+        else:
+            print("\nYou already brushed your teeth.  If your brush them too much, you'll end up getting cavities (weirdly enough).")
+
+    elif current == "SINK" and s == "MEDS":
+        if len(inventory) == 2:
+            inventory.append("TOOK MEDS")
+            print("You successfully TOOK your MEDS\n Take that, depression and anxiety!")
+        elif len(inventory) == 0:
+            print("\nUgh, you're too distracted by how gross you feel.  Should probably shower first.")
+        elif len(inventory) == 1:
+            print("\nThe taste of your mouth is getting the better of you.  Better take care of that.")
+        else:
+            print("\nYou already took your meds.  They'll kick in soon.")
+
+    elif current == "SINK" and s == "FACE":
+        if len(inventory) == 3:
+            inventory.append("WASHED FACE")
+            print("You successfully WASHED your FACE\n Take that, oils and acne!")
+        elif len(inventory) == 0:
+            print("\nIf you wash your face before showering, all the moisturizer in your soap will get washed out.  It's better to\n shower first.")
+        elif len(inventory) == 1:
+            print("\nYou've always been messy when brushing your teeth.  Better do that first, then you can rinse off the paste\n from your mouth")
+        elif len(inventory) == 2:
+            print("\nIt's usually best to take your meds after brushing your teeth. \nThat way you don't have to wait for the water to cool back down to a drinking temperature.")
+        else:
+            print("\nYou already washed your face.  Why would you wash out the moisturizer?")
+
+    elif current == "MIRROR" and s == "MAKEUP":
+        if len(inventory) == 4:
+            inventory.append("APPLIED MAKEUP")
+            print("You successfully APPLIED your MAKEUP\nHave to make sure the world doesn't know just how hard you\nwent last night.")
+        elif len(inventory) == 0:
+            print("\nYou really need to shower.")
+        elif len(inventory) == 1:
+            print("\nYou can't just skip brushing your teeth.")
+        elif len(inventory) == 2:
+            print("\nYou need to take your meds before you forget.")
+        elif len(inventory) == 3:
+            print("\nYou have to wash your face before putting on makeup.")
+        else:
+            print("\nYou have enough makeup on.")
+            
+    elif current == "MIRROR" and s == "HAIR":
+        if len(inventory) == 5:
+            inventory.append("STYLED HAIR")
+            print("You successfully STYLED your HAIR\nIf your hair looks good, no one will know just how messy you really are!")
+        elif len(inventory) == 0:
+            print("\nYou're hair is too gross to style.'")
+        elif len(inventory) == 1:
+            print("\nYou should brush your teeth first.")
+        elif len(inventory) == 2:
+            print("\nYou need to take your meds before you forget.")
+        elif len(inventory) == 3:
+            print("\nYou have to wash your face first.")
+        elif len(inventory) == 4:
+            print("\nYou should do your makeup first, since your hair needs to be pulled back for that.")    
+        else:
+            print("\nYour hair is styled enough.")
+
+    elif current == "OPEN" and s == "DRESS":
+        if len(inventory) == 6:
+            inventory.append("GOT DRESSED")
+            print("You successfully GOT DRESSED\nNow you are ready to face the day!")
+            current = 'END'
+        elif len(inventory) == 0:
+            print("\nUgh, you still smell like the bar from last night.  Should probably take care of that.")
+        elif len(inventory) == 1:
+            print("\nYou should brush your teeth first.")
+        elif len(inventory) == 2:
+            print("\nYou need to take your meds before you forget.")
+        elif len(inventory) == 3:
+            print("\nYou have to wash your face first.")
+        elif len(inventory) == 4:
+            print("\nYou should do your makeup first, so you don't get it on your clothes")
+        elif len(inventory) == 5:
+            print("\nYou've made the mistake of doing you hair after getting dressed in the past, and ended up with hairspray all\nover your clothes.")
+        
+
     else:
         for e in game['rooms'][current]['exits']:
             if s == e['verb'] and e['target'] != 'NoExit':
                 return e['target']
-    print("\nYou can't go that way!")
+
+    
     return current
 
 
@@ -56,7 +150,7 @@ def update(selection,game,current,inventory):
 
 def printExits(game,current):
     e = ", ".join(str(x['verb']) for x in game['rooms'][current]['exits'])
-    print('\nYou can go the following directions: {directions}'.format(directions = e))
+    print('\nYou can move to the following: {directions}'.format(directions = e))
 
 def normalizeVerb(selection,verbs):
     for v in verbs:
@@ -64,13 +158,13 @@ def normalizeVerb(selection,verbs):
             return v['map']
     return ""
 
-def end_game(winning,points,moves):
+def end_game(winning,moves):
     if winning:
         print('You have won! Congratulations')
-        print('You scored {points} points in {moves} moves! Nicely done!'.format(moves=moves, points=points))
+        print('You finished in {moves} moves! Nicely done!'.format(moves=moves))
     else:
         print('Thanks for playing!')
-        print('You scored {points} points in {moves} moves. See you next time!'.format(moves=moves, points=points))
+        print('You finished in {moves} moves. See you next time!'.format(moves=moves))
 
 
 
@@ -87,26 +181,25 @@ def main():
     win = ['END']
     lose = []
     moves = 0
-    points = 0
     inventory = []
 
     while True:
 
-        render(game,current,moves,points)
+        render(game,current,moves)
 
         selection = getInput(game,current,game['verbs'])
 
         if selection[0] == 'QUIT':
-            end_game(False,points,moves)
+            end_game(False,moves)
             break
 
         current = update(selection,game,current,inventory)
 
         if current in win:
-            end_game(True,points,moves)
+            end_game(True,moves)
             break
         if current in lose:
-            end_game(False,points,moves)
+            end_game(False,moves)
             break
 
         moves += 1
